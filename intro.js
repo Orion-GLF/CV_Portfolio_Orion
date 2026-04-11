387515668c309bf7ac6ff799a6241ad1f472ea7e
Orion-GLF <garofalo.orion@gmail.com>
2026-04-12 01:15:11 +0200

    intro.js fini

--- intro.js
+++ intro.js
@@ -1,12 +1,24 @@
 const introScreen = document.getElementById("intro-screen");
 const introEnterButton = document.getElementById("intro-enter");
 
+let introClosed = false;
+
 function hideIntroScreen() {
+  if (introClosed) return;
+  introClosed = true;
+
   introScreen.classList.add("intro-hide");
 
   setTimeout(() => {
     introScreen.style.display = "none";
   }, 1100);
 }
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    hideIntroScreen();
+  }
+});
 
 introEnterButton.addEventListener("click", hideIntroScreen);
